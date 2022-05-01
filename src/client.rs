f3889e01fb6fb1e51adebbb7500488937266afcd
kate <kate@dariox.club>
2022-05-01 12:50:47 +0800

    Add function get_all_zone_records

--- src/client.rs
+++ src/client.rs
@@ -5,66 +5,66 @@ use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseResultInfo
 {
-    page: i32,
-    per_page: i32,
-    total_pages: i32,
-    count: i32,
-    total_count: i32
+    pub page: i32,
+    pub per_page: i32,
+    pub total_pages: i32,
+    pub count: i32,
+    pub total_count: i32
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseZoneInfoOwner
 {
-    id: String,
-    email: String
+    pub id: String,
+    pub email: String
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseZoneInfoAccount
 {
-    id: String,
-    name: String
+    pub id: String,
+    pub name: String
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseZoneInfoMeta
 {
-    step: i32,
-    custom_certificate_quota: i32,
-    page_rule_quota: i32,
-    phishing_detected: bool,
-    multiple_railguns_allowed: bool
+    pub step: i32,
+    pub custom_certificate_quota: i32,
+    pub page_rule_quota: i32,
+    pub phishing_detected: bool,
+    pub multiple_railguns_allowed: bool
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseZonePlan
 {
-    id: String,
-    name: String,
-    price: Option<i32>,
-    currency: Option<String>,
-    frequency: Option<String>,
-    is_subscribed: Option<bool>,
-    can_subscribe: Option<bool>,
-    legacy_id: Option<String>,
-    legacy_discount: Option<bool>,
-    externally_managed: Option<bool>
+    pub id: String,
+    pub name: String,
+    pub price: Option<i32>,
+    pub currency: Option<String>,
+    pub frequency: Option<String>,
+    pub is_subscribed: Option<bool>,
+    pub can_subscribe: Option<bool>,
+    pub legacy_id: Option<String>,
+    pub legacy_discount: Option<bool>,
+    pub externally_managed: Option<bool>
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseZoneInfo
 {
-    id: String,
-    name: String,
-    status: String,
-    paused: bool,
-    development_mode: i32,
-    name_servers: Vec<String>,
-    original_name_servers: Option<Vec<String>>,
-    original_registrar: Option<String>,
-    original_dnshost: Option<String>,
-    modified_on: String,
-    activated_on: String,
-    meta: ResponseZoneInfoMeta,
-    owner: ResponseZoneInfoOwner,
-    account: ResponseZoneInfoAccount,
-    permissions: Vec<String>,
-    plan: Option<ResponseZonePlan>
+    pub id: String,
+    pub name: String,
+    pub status: String,
+    pub paused: bool,
+    pub development_mode: i32,
+    pub name_servers: Vec<String>,
+    pub original_name_servers: Option<Vec<String>>,
+    pub original_registrar: Option<String>,
+    pub original_dnshost: Option<String>,
+    pub modified_on: String,
+    pub activated_on: String,
+    pub meta: ResponseZoneInfoMeta,
+    pub owner: ResponseZoneInfoOwner,
+    pub account: ResponseZoneInfoAccount,
+    pub permissions: Vec<String>,
+    pub plan: Option<ResponseZonePlan>
 }
 
 pub struct Client
@@ -102,7 +102,7 @@ impl Client
     }
 
     #[allow(dead_code)]
-    pub async fn get_zone(&mut self, zone: String) -> Result<String, task::JoinError>
+    pub async fn get_zone(&mut self, zone: String) -> Result<Option<ResponseZoneInfo>, task::JoinError>
     {
         let resp: std::result::Result<reqwest::Response, reqwest::Error> =
             self.web_client.get([
@@ -115,6 +115,40 @@ impl Client
             
         let thing: reqwest::Result<String> = resp.unwrap().text().await;
         let result = thing.unwrap();
+
+        let mut final_response: Option<ResponseZoneInfo> = None;
+
+        let response_data: serde_json::Value = serde_json::from_str(&result).unwrap();
+        if response_data["success"] == true
+        {
+            let result_data: ResponseZoneInfo = serde_json::from_str(&response_data["result"].to_string()).unwrap();
+            final_response = Option::Some(result_data);
+        }
+
+        Ok(final_response)
+    }
+
+    pub async fn get_all_zone_records(&mut self, zone: String) -> Result<String, task::JoinError>
+    {
+        let resp: std::result::Result<reqwest::Response, reqwest::Error> = 
+            self.web_client.get([
+                self.base_url.to_string(),
+                String::from("zones"),
+                zone,
+                String::from("dns_records")
+            ].join("/"))
+            .headers(self.auth.headers())
+            .send()
+            .await;
+        let thing: reqwest::Result<String> = resp.unwrap().text().await;
+        let result: String = thing.unwrap();
+
+        let response_data: serde_json::Value = serde_json::from_str(&result).unwrap();
+        if response_data["success"] == true
+        {
+            let result_data: serde_json::Value = serde_json::from_str(&response_data["result"].to_string()).unwrap();
+        }
+
         Ok(result)
     }
 
