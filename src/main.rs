dff7bebbe93f03ca63b592de52676e9e45483d69
kate <kate@dariox.club>
2022-07-18 13:19:15 +0800

    Display available zones

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,19 @@ mod auth;
 mod client;
 mod configparse;
 use tokio::task;
+use std::{collections::{HashMap}, array};
+use serde_json::{Value};
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+#[derive(Deserialize, Debug)]
+struct Item {
+    value: u64,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), task::JoinError>
@@ -34,6 +47,20 @@ async fn main() -> Result<(), task::JoinError>
 
     let verify_response: String = client.verify_token().await.unwrap();
     println!("verifyResponse: {}", verify_response);
+
+    let getZoneArrRes: String = client.get_zone_arr().await.unwrap();
+    let getZoneArrRes_str: &str = &getZoneArrRes[..];
+    let getZoneArrRes_map: HashMap<String, Value> = serde_json::from_str(getZoneArrRes_str).unwrap();
+    println!("getZoneArrRes: {:#?}", getZoneArrRes_map);
+    println!("success: {}", getZoneArrRes_map.get(&String::from("success")).unwrap());
+    let responseArr = getZoneArrRes_map.get(&String::from("result")).unwrap().as_array().unwrap();
+    
+    for (i, elem) in responseArr.iter().enumerate() {
+        let name = elem.get(&String::from("name")).unwrap().to_string();
+        let id = elem.get(&String::from("id")).unwrap().to_string();
+        print!("{}: ", id);
+        print!("{}\n", name);
+    }
 
     Ok(())
 }
